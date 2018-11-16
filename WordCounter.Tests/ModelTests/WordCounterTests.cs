4f72c3af938c6865720bc5fb234203448518afbe
Sheila Stephen <sheila.je@gmail.com>
2018-11-16 09:12:22 -0800

    Create the file structure and install the packages

---
+++ WordCounter.Tests/ModelTests/WordCounterTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordCounter.Models;
+
+namespace WordCounter.Tests
+{
+  [TestClass]
+  public class WordCounterTest
+  {
+
+    // Test methods will go here.
+
+  }
+}
