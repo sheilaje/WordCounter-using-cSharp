09ca1b376668090b18b90e8677f267f5c05f6268
Sheila Stephen <sheila.je@gmail.com>
2018-11-16 11:15:07 -0800

    Add test to check the datatype of the class and the created object

--- WordCounter.Tests/ModelTests/WordCounterTests.cs
+++ WordCounter.Tests/ModelTests/WordCounterTests.cs
@@ -1,13 +1,37 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using WordCounter.Models;
+//using System.Collections.Generic;
 
-namespace WordCounter.Tests
+namespace WordCounter.Models
 {
   [TestClass]
   public class WordCounterTest
   {
-
-    // Test methods will go here.
-
+    [TestMethod]
+    public void WordCounterConstructor_CreatesInstanceOfWordCounter_WordCounter()
+    {
+      WordCounter testWordCounter = new WordCounter("test");
+      Assert.AreEqual(typeof(WordCounter), testWordCounter.GetType());
+    }
   }
 }
+
+// using Microsoft.VisualStudio.TestTools.UnitTesting;
+// using ToDoList.Models;
+//
+// namespace ToDoList.Tests
+// {
+//   [TestClass]
+//   public class ItemTest
+//   {
+//
+//     [TestMethod]
+//     public void ItemConstructor_CreatesInstanceOfItem_Item()
+//     {
+//       WC testWordCounter = new WC();
+//       Assert.AreEqual(typeof(WC), testWordCounter.GetType());
+//     }
+//
+//   }
+// }
---
+++ WordCounter/Models/WordCounter.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace WordCounter.Models
+{
+  public class WordCounter
+  {
+    // private string _word;
+    // private string _sentence;
+    public WordCounter(string word)
+    {
+      // _word = word;
+    }
+  }
+}
